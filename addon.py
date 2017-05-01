06f2b6a7d5f3ccab8c74a85ff2b694ff43fbfd80
jester-xbmc <jester@ocs.nl>
2017-05-01 11:15:02 +0200

    Bump to 1.0.3

--- addon.py
+++ addon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#     Copyright (C) 2016 Jester
+#     Copyright (C) 2016,2017 Jester
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from xbmcswift2 import Plugin, xbmc
+from kodiswift import Plugin, xbmc
 
 plugin = Plugin()
 
@@ -26,14 +26,32 @@ def show_slam_list():
     items = [
 
   {'label': 'SLAM! TV Stream',
-   'thumbnail': 'special://home/addons/plugin.video.slam/icon.png',
+   'icon': 'special://home/addons/plugin.video.slam/icon.png',
    'path': 'http://hls2.slamfm.nl/content/slamtv/slamtv_1.m3u8',
    'is_playable': True,
   },
   
   {'label': 'SLAM! Webcam Live Stream',
-   'thumbnail': 'special://home/addons/plugin.video.slam/icon.png',
-   'path': 'http://hls2.slamfm.nl/content/slamwebcam/slamwebcam_1.m3u8',
+   'icon': 'special://home/addons/plugin.video.slam/icon.png',
+   'path': 'https://hls.slam.nl/streaming/hls/SLAM!/playlist.m3u8',
+   'is_playable': True,
+  },
+  
+  {'label': 'SLAM! Live Stream (Audio only)',
+   'icon': 'special://home/addons/plugin.video.slam/icon.png',
+   'path': 'http://stream.slam.nl/slam',
+   'is_playable': True,
+  },
+  
+  {'label': 'SLAM! Non-Stop Stream (Audio only)',
+   'icon': 'special://home/addons/plugin.video.slam/icon.png',
+   'path': 'https://streaming.slam.nl/web10_mp3',
+   'is_playable': True,
+  },
+  
+  {'label': 'SLAM! Juize Stream (Audio only)',
+   'icon': 'special://home/addons/plugin.video.slam/icon.png',
+   'path': 'https://streaming.slam.nl/web09_mp3',
    'is_playable': True,
   },
 ]
